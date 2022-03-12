062d9598af072313a002a6f09083d8391ede12ec
Pedro Paulo Costa Nogueira <71938483+ppcnogueira@users.noreply.github.com>
2022-03-12 10:39:01 -0300

    Código para controlar o número de processos
    Código para controlar o número de processos recebidos

--- contratacao_covid.py
+++ contratacao_covid.py
@@ -1,35 +1,27 @@
-# Controle de processos de Covid
+# Controle de processos estratégicos
 # Cuidado ao executar esta célula pois reseta o número de processos nas listas!!!!
 
-# Meses/Semanas para controle
-outubro_1 = ['2000572909', '2000511220', '2000553197', '2000578039', '2000576949', '2000572909', '2000572815',
-             '2000566766', '2000591286', '2000591010', '2000576774', '2000593839',
-             '2000576708', '2000593477', '2000576821', '2000577073', '2000593051', '2000592926', '2000593607',
-             '2000588052', '2000562164', ]
-outubro_2 = []
-outubro_3 = []
-outubro_4 = []
+# Meses/Semanas para controle / Inserir em cada lista a númeração dos processos
+mês_semana1 = []
+mês_semana2 = []
+mês_semana3 = []
+mês_semana4 = []
 
 # Utilizar o for para verificar o total de processos recebidos na respectiva semana
 i = i2 = i3 = i4 = 0
 
-for gcpj in outubro_1:
-    # print(gcpj)
+for processo in mês_semana1:
     i = i + 1
-for gcpj in outubro_2:
-    # print(gcpj)
+for processo in mês_semana2:
     i2 = i2 + 1
-for gcpj in outubro_3:
-    # print(gcpj)
+for processo in mês_semana3:
     i3 = i3 + 1
-for gcpj in outubro_4:
-    # print(gcpj)
+for processo in mês_semana4:
     i4 = i4 + 1
 
-# Inserir neste input os GCPJs recebidos
-outubro_2.append(str(input('Digite o GCPJ recebido: ')))
-for gcpj in outubro_2:
-  #print(gcpj)
+# Inserir neste input os processos recebidos
+mês_semana1.append(str(input('Digite o número do processo recebido: ')))
+for processo in mês_semana1:
   i2 = i2 + 1
 
 print(f'Recebemos na 1º semana de Outubro {i} processos.')
@@ -37,10 +29,3 @@ print(f'Recebemos na 2º semana de Outubro {i2} processos.')
 print(f'Recebemos na 3º semana de Outubro {i3} processos.')
 print(f'Recebemos na 4º semana de Outubro {i4} processos.')
 print(f'Recebemos em Outubro um total de {i + i2 + i3 + i4} processos.')
-
-# Verificar as listas
-
-#print(outubro_1)
-#print(outubro_2)
-#print(outubro_3)
-#print(outubro_4)
